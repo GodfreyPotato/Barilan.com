7ed4fedb3b6aab9a92d99125778228484dfb4fb7
rinkaji <rinkajhin@gmail.com>
2025-03-12 11:00:58 +0800

    game over background

--- lib/flame_game/gameScreen.dart
+++ lib/flame_game/gameScreen.dart
@@ -24,25 +24,131 @@ class _GameScreenState extends State<GameScreen> {
         ),
         overlayBuilderMap: {
           'backBut':
-              (context, game) => Column(
+              (context, game) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Provider.of<Playerdata>(context, listen: false).reset();
-                    },
-                    child: Icon(Icons.play_arrow),
+                  Column(
+                    spacing: 12,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                          "Score: 21312",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ),
+                      Health(),
+                    ],
                   ),
-                  Health(),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          Provider.of<Playerdata>(
+                            context,
+                            listen: false,
+                          ).reset();
+                        },
+                        child: Icon(Icons.home),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Icon(Icons.restart_alt),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Icon(Icons.music_off),
+                      ),
+                    ],
+                  ),
                 ],
               ),
           'gameOver':
               (context, game) => Center(
                 child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
                   width: 200,
                   height: 200,
-                  color: Colors.red,
-                  child: Center(child: Text("Game Over")),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset("assets/bg/gameOver2.png"),
+                      Text(
+                        "Score: 12",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Row(
+                        spacing: 12,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 0.0),
+                            child: Container(
+                              alignment: Alignment.center,
+                              width: 80,
+                              height: 25,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.white60,
+                                ),
+                                color: Colors.deepPurple,
+                                borderRadius: BorderRadius.circular(13),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Text(
+                                  "Home",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 0.0),
+                            child: Container(
+                              alignment: Alignment.center,
+                              width: 80,
+                              height: 25,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.white60,
+                                ),
+                                color: Colors.deepPurple,
+                                borderRadius: BorderRadius.circular(13),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Text(
+                                  "Try Again",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
         },
